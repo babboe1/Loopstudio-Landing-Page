f83841b5b916ba98854be7608725e1e7e5a12815
babboe1 <ayodelaustinab@gmail.com>
2022-01-24 06:32:19 +0100

    feat: create event function on click

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,1 +1,7 @@
 const BACKDROP = document.getElementById('backdrop');
+
+BACKDROP.addEventListener('click', BackDropHandler);
+MENU_BTN.addEventListener('click', MenuBtnHandler);
+CLOSE_BTN.addEventListener('click', CloseBtnHandler);
+ACTION_BTN_MOBILE.addEventListener('click', ActionBtnHandler);
+ACTION_BTN_DESKTOP.addEventListener('click', ActionBtnHandler);
