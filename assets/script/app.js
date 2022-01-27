3a4e5f9be4c814067faf18c2126dd8d22c072e7f
babboe1 <ayodelaustinab@gmail.com>
2022-01-27 02:40:57 +0100

    refactor: create constant DOM variables

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,4 +1,16 @@
 const BACKDROP = document.getElementById('backdrop');
+const MENU_BTN = document.querySelector('.MenuBtn');
+const CLOSE_BTN = document.getElementById('close-btn');
+const ACTION_BTN_DESKTOP = document.querySelector('.ActionBtn.Desktop').firstElementChild;
+const ACTION_BTN_MOBILE = document.querySelector('.ActionBtn.Mobile').firstElementChild;
+const MENU_OPTION = document.querySelector('.MenuOpt').parentElement;
+const IMAGE_BOXES = document.querySelectorAll('.CreationImgBox');
+const IMAGE_BOX = document.querySelectorAll('.CreationImgBox.auto');
+const ACTION_BTN_TEXT = document.querySelectorAll('.ActionText');
+const BG_SLIDE = document.querySelector('.CreationImgBox.BgSlide');
+const CREATION_CONTENT = document.querySelector('.CreationContent');
+const TEXT_CONTENT = document.querySelectorAll('.TextContent');
+const TEXT_HOLDER = document.querySelectorAll('.TextHolder');
 
 BACKDROP.addEventListener('click', BackDropHandler);
 MENU_BTN.addEventListener('click', MenuBtnHandler);
