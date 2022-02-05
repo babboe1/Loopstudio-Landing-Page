3c7c8498c896370e7a519da91d0f322638bb49b0
babboe1 <ayodelaustinab@gmail.com>
2022-02-05 10:50:58 +0100

    refactor: format code

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,8 +1,10 @@
 const BACKDROP = document.getElementById('backdrop');
 const MENU_BTN = document.querySelector('.MenuBtn');
 const CLOSE_BTN = document.getElementById('close-btn');
-const ACTION_BTN_DESKTOP = document.querySelector('.ActionBtn.Desktop').firstElementChild;
-const ACTION_BTN_MOBILE = document.querySelector('.ActionBtn.Mobile').firstElementChild;
+const ACTION_BTN_DESKTOP =
+	document.querySelector('.ActionBtn.Desktop').firstElementChild;
+const ACTION_BTN_MOBILE =
+	document.querySelector('.ActionBtn.Mobile').firstElementChild;
 const MENU_OPTION = document.querySelector('.MenuOpt').parentElement;
 const IMAGE_BOXES = document.querySelectorAll('.CreationImgBox');
 const IMAGE_BOX = document.querySelectorAll('.CreationImgBox.auto');
