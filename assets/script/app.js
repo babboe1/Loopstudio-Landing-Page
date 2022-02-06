51e3f0e1d206ba06e5b4515852a07ce75af959a1
babboe1 <ayodelaustinab@gmail.com>
2022-02-06 11:01:25 +0100

    refactor: format code

--- assets/script/app.js
+++ assets/script/app.js
@@ -83,10 +83,10 @@ const ActionBtnHandler = () => {
 				ImgBoxes.classList.add('hidden');
 			}
 			Text.textContent = 'SEE ALL';
-         BG_SLIDE.classList.remove('hidden');
-         CREATION_CONTENT.classList.remove('animate_easy');
-      } else if (Text.textContent === 'COLLAPSE' && window.innerWidth > 767) {
-         for (const ImgBoxes of IMAGE_BOXES) {
+			BG_SLIDE.classList.remove('hidden');
+			CREATION_CONTENT.classList.remove('animate_easy');
+		} else if (Text.textContent === 'COLLAPSE' && window.innerWidth > 767) {
+			for (const ImgBoxes of IMAGE_BOXES) {
 				ImgBoxes.classList.add('hidden');
          }
          for (const ImgBox of IMAGE_BOX) {
